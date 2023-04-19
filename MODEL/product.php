81689ca8cf0343a418e2169cae8e00d49299e35b
AlbertoPavarin <albertopavarin@libero.it>
2023-04-19 12:19:00 +0200

    set e update product

--- MODEL/product.php
+++ MODEL/product.php
@@ -53,5 +53,69 @@ class Product
 
         return self::$conn->query($sql);
     }
+
+
+    /*{
+        "description": "Sapone per vetri",
+        "quantity": 10,
+        "price": 5,
+        "name": "Vetril"
+    }*/
+
+    public static function setProduct($description, $quantity, $price, $name)
+    {
+        $sql = "INSERT INTO product (description, quantity, price, nome)
+                VALUES (?, ?, ?, ?);";
+
+        $stmt = self::$conn->prepare($sql);
+        $stmt->bind_param('sids', $description, $quantity, $price, $name);
+        return $stmt->execute();
+    } 
+
+    public static function updateProduct($description, $quantity, $price, $name, $id) 
+    {
+        $sql = "UPDATE product
+                SET 
+                    description = ?,
+                    quantity = ?, 
+                    price = ?,
+                    nome = ?
+                WHERE id = ?;";
+
+        $stmt = self::$conn->prepare($sql);
+        $stmt->bind_param('sidsi', $description, $quantity, $price, $name, $id);
+        if ($stmt->execute())
+        {
+            return $stmt;
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    public static function deleteProduct($id)
+    {
+        $sql = "UPDATE product
+                SET
+                    active = 0
+                WHERE id = ?;";
+        
+        $stmt = self::$conn->prepare($sql);
+        $stmt->bind_param('i', $id);
+        if ($stmt->execute())
+        {
+            return $stmt;
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    public static function reactiveProduct()
+    {
+        
+    }
 }
 ?>
