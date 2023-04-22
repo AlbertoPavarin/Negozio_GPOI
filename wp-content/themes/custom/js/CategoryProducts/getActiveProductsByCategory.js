79821775a14f91f597af7b197818cb0ac7f1c270
AlbertoPavarin <albertopavarin@libero.it>
2023-04-22 10:26:29 +0200

    inizio pagina catalogo

---
+++ wp-content/themes/custom/js/CategoryProducts/getActiveProductsByCategory.js
@@ -0,0 +1,18 @@
+function getActiveProductsByCategory(id_cat) {
+    let result = "";
+
+    $.ajax({
+        url: `/Negozio_GPOI/backend/API/category_product/getProductsByCategory.php?category_id=${id_cat}`,
+        type: 'GET',
+        dataType: 'json',
+        async: false,
+        success: (data) => {
+            result = data;
+        },
+        error: () => {
+            result = "404";
+        }
+    });
+
+    return result;
+}
--- wp-content/themes/custom/page-catalogo.php
+++ wp-content/themes/custom/page-catalogo.php
@@ -1,3 +1,8 @@
 <?php
 require_once('page.php');
 ?>
+<script type="text/javascript" src="/Negozio_GPOI/wp-content/themes/custom/js/CategoryProducts/getActiveProductsByCategory.js"></script>
+
+<script>
+    console.log(getActiveProductsByCategory(<?php echo $_GET["id"] ?>));
+</script>
