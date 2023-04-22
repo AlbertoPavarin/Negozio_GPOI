3186b219a0a5610509aed0b877784725c66020bb
AlbertoPavarin <albertopavarin@libero.it>
2023-04-22 21:13:14 +0200

    catalogo categoria

--- wp-content/themes/custom/page-catalogo.php
+++ wp-content/themes/custom/page-catalogo.php
@@ -1,8 +1,36 @@
 <?php
 require_once('page.php');
+
+if (empty($_GET["id"]))
+{
+    die("Errore nel caricamento del catalogo");
+}
 ?>
+
 <script type="text/javascript" src="/Negozio_GPOI/wp-content/themes/custom/js/CategoryProducts/getActiveProductsByCategory.js"></script>
 
 <script>
-    console.log(getActiveProductsByCategory(<?php echo $_GET["id"] ?>));
+    let products = getActiveProductsByCategory(<?php echo $_GET["id"] ?>);
+    console.log(products);
 </script>
+
+<div class="container">
+    <div class="row prods-cont">
+        <script>
+            let i = 0;
+            products.forEach((product) => {
+                const catDiv = document.createElement('div');
+                catDiv.classList = "col-md-5 product-container mt-3 mb-4 p-3";
+                catDiv.innerHTML = `<a class="a-cat" href="/Negozio_GPOI/prodotto?id=${product.id}">${product.nome}</a>
+                                    <p>${product.price}€</p>`;
+                document.querySelector('.prods-cont').appendChild(catDiv);
+                if (i % 2 == 0) {
+                    const catDivc = document.createElement('div');
+                    catDivc.classList = "col-md-2";
+                    document.querySelector('.prods-cont').appendChild(catDivc);
+                }
+                i += 1;
+            })
+        </script>
+    </div> 
+</div>
