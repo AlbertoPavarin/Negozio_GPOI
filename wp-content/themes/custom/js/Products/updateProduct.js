7fdde02b576a20922f05ff42da9e3cd8e541ce13
AlbertoPavarin <albertopavarin@libero.it>
2023-04-26 12:05:58 +0200

    .

---
+++ wp-content/themes/custom/js/Products/updateProduct.js
@@ -0,0 +1,26 @@
+function updateProduct(id, name, description, price, quantity)
+{
+    let res = "";
+    $.ajax({
+        url: `/Negozio_GPOI/backend/API/product/updateProduct.php`,
+        type: 'POST',
+        headers : {'Content-Type':'application/json; charset=utf-8'},
+        dataType: 'json',
+        async: false,
+        data: 
+           JSON.stringify({
+                id: id,
+                name: name,
+                description: description, 
+                price: price,
+                quantity: quantity
+            }),
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (error) {
+            res = "400";
+        }
+    });
+    return res;
+}
--- wp-content/themes/custom/page-modifica-prodotto.php
+++ wp-content/themes/custom/page-modifica-prodotto.php
@@ -16,10 +16,11 @@ endif;
 ?>
 
 <script type="text/javascript" src="/Negozio_GPOI/wp-content/themes/custom/js/Products/getProduct.js"></script>
+<script type="text/javascript" src="/Negozio_GPOI/wp-content/themes/custom/js/Products/updateProduct.js"></script>
 
-<div class="container-fluid">
+<div class="container">
     <div class="row justify-content-center text-center">
-        <div class="col-6 insert-form">
+        <div class="col-0 insert-form">
             <form class="justify-content-center mt-3" id="form-insert-device">
 
                 <div class="justify-content-center insert-input">
@@ -32,13 +33,18 @@ endif;
                     <input type="text" name="description" id="description" class="form-control">
                 </div>
 
+                <div class="justify-content-center insert-input mt-3">
+                    <label for="description">Prezzo</label>
+                    <input type="number" name="price" id="price" class="form-control">
+                </div>
+
                 <label class="mt-3" for="name">Categoria</label>
                 <div class="d-flex justify-content-center insert-input">
                     <select name="classs" id="class-select" class="form-select"></select>
                 </div>
 
                 <div id="cont-sel"></div>
-                <input type="button" value="Modifica prodotto" id="create-dev" class="btn btn-primary mt-3 w-100" onclick="">
+                <input type="button" value="Modifica prodotto" id="update-prod" class="btn btn-primary mt-3 w-100">
                 <p id="error-msg"></p>
             </form>
         </div>
@@ -48,4 +54,18 @@ endif;
 <script>
     let product = getProduct(<?php echo $_GET["id"] ?>);
     console.log(product);
+
+    let inputName = document.querySelector('#name');
+    inputName.value = product.nome;
+
+    let inputDescription = document.querySelector('#description');
+    inputDescription.value = product.description;
+
+    let inputPrice = document.querySelector('#price');
+    inputPrice.value = product.price;
+
+    let btnUpdate = document.querySelector('#update-prod');
+    btnUpdate.onclick = () => {
+        updateProduct(product.id, inputName.value, inputDescription.value, inputPrice.value, product.quantity);
+    }
 </script>
